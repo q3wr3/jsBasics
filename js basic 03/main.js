0cf1c65706caa5914ea843ec9917b604e9cc012b
Marko <marko@tolevski.com>
2021-08-12 11:49:53 +0200

    tret chas...

---
+++ js basic 03/main.js
@@ -0,0 +1,53 @@
+let str1 = "stifler's mom";
+let str2 = "stringot ima 'edinechni' navodnici";
+let str3 = 'stringot ima "dupli" navodnici';
+
+
+let strL = '0123456789';
+console.log('stringot ima ' + strL.length + ' karakteri');
+
+/*
+let str4 = "Mile voli disko...";
+
+let delche = str4.slice(4,18); //kako parametri se vnesuvaat poziciite za sechenje
+let delche1 = str4.slice(4);
+console.log(str4 + " -> " +delche+ " bez vtor parametar -> " + delche1)
+
+console.log("so negativni vrednsti ->" + str4.slice(-13,-9)) //broenje od krajot na stringot
+
+console.log(str4.substring(5,16)) // ne prima negativni vrednosti
+
+console.log(str4.substr(5,9)) //prv parametar - pochetna pozicija, vtor parametar - broj na mesta posle pochetnata pozicija
+
+
+let zaZamena = "Vlada na RM, odluchi deka na 16ti RM ke bidi zatvorena za poseta od vonzemjani"
+
+console.log("pred :"+zaZamena+" | potoa: "+zaZamena.replace('RM','SRM'))
+
+//zamena so REGEX (regular expression)
+
+var zamenat = zaZamena.replace(/RM/g,'SRM')
+
+console.log('rezultat so REgEx:',zamenat)
+
+console.log('so golemi:',zamenat.toUpperCase())
+console.log('so mali:',zamenat.toLowerCase())
+
+var conkatanacija = zaZamena.concat("&&",zamenat," tret text")
+console.log(conkatanacija)
+
+
+
+//trim
+
+let tekst = "                Pece         ";
+
+console.log(tekst.trim())
+
+
+
+//trim
+
+let cel = "pece,kire,ace,stojan,krume"
+let delnat = cel.split(',')
+console.log(delnat,delnat[4])*/
