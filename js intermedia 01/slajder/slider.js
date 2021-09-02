c37c6c9a0420fa76102205b2b6ad498d74fa58f6
Marko <marko@tolevski.com>
2021-09-02 12:03:10 +0200

    Slider Finish

--- js intermedia 01/slajder/slider.js
+++ js intermedia 01/slajder/slider.js
@@ -1,11 +1,41 @@
 document.querySelector('.navPrev').addEventListener('click',function(){
-	var slider = document.querySelector('.slides')
-	var fullW = slider.scrollWidth
-	var allSlides = slider.children.length;
-	var sliding = fullW / allSlides;
 
-	var pos = slider.getBoundingClientRect();
-	var currentSlide = slider.querySelector('.active')
-	var newLeft = parseInt(slider.style.left) - sliding;
-	
+	let {sliderLeft,currentSlide,hasNext,hasPrev} = getParams()
+	console.log(sliderLeft,currentSlide,hasNext,hasPrev)
+	if (hasPrev){
+		let slideW = currentSlide.getBoundingClientRect().width
+		let newLeft = sliderLeft + slideW;
+		document.querySelector('.slides').style.transform = 'translateX('+newLeft+'px)';
+		currentSlide.previousElementSibling.classList.add('active')
+		currentSlide.classList.remove('active')
+	}
+
 })
+
+
+document.querySelector('.navNext').addEventListener('click',function(){
+
+	let {sliderLeft,currentSlide,hasNext,hasPrev} = getParams()
+	if (hasNext){
+		let slideW = currentSlide.getBoundingClientRect().width
+		let newLeft = sliderLeft - slideW;
+		document.querySelector('.slides').style.transform = 'translateX('+newLeft+'px)';
+		currentSlide.nextElementSibling.classList.add('active')
+		currentSlide.classList.remove('active')
+	}
+
+})
+
+
+function getParams(){
+	var parent = document.querySelector('.slides')
+	var currentSlide = document.querySelector('.active')
+	var hasNext = currentSlide.nextElementSibling != null;
+	var hasPrev = currentSlide.previousElementSibling != null;
+	return {
+		currentSlide : currentSlide,
+		hasNext : hasNext,
+		hasPrev : hasPrev,
+		sliderLeft : parent.getBoundingClientRect().left
+	}
+}
